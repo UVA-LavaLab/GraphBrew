f8624a3f377479065c1fe87ef76c240d43309568
Abdullah <atmughrabi@gmail.com>
2024-07-18 04:25:13 -0400

    [GBrew] fix util inline issue

--- bench/include/gapbs/util.h
+++ bench/include/gapbs/util.h
@@ -52,22 +52,22 @@ template <typename T> T *alloc_align_4k(size_t size)
     return static_cast<T *>(ptr);
 }
 
-void PrintLabel(const std::string &label, const std::string &val)
+inline void PrintLabel(const std::string &label, const std::string &val)
 {
     printf("%-21s%7s\n", (label + ":").c_str(), val.c_str());
 }
 
-void PrintTime(const std::string &s, double seconds)
+inline void PrintTime(const std::string &s, double seconds)
 {
     printf("%-21s%3.5lf\n", (s + ":").c_str(), seconds);
 }
 
-void PrintStep(const std::string &s, int64_t count)
+inline void PrintStep(const std::string &s, int64_t count)
 {
     printf("%-14s%14" PRId64 "\n", (s + ":").c_str(), count);
 }
 
-void PrintStep(const std::string &s, double seconds, int64_t count = -1)
+inline void PrintStep(const std::string &s, double seconds, int64_t count = -1)
 {
     if (count != -1)
         printf("%5s%11" PRId64 "  %10.5lf\n", s.c_str(), count, seconds);
@@ -75,7 +75,7 @@ void PrintStep(const std::string &s, double seconds, int64_t count = -1)
         printf("%5s%23.5lf\n", s.c_str(), seconds);
 }
 
-void PrintStep(int step, double seconds, int64_t count = -1)
+inline void PrintStep(int step, double seconds, int64_t count = -1)
 {
     PrintStep(std::to_string(step), seconds, count);
 }
